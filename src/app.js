c81a5d90bdd8561b4fbd0ad99f616c4f8799f637
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2016-06-02 01:10:18 +0700

    Update React state in a component
    - fix api using Webpack's file loader

--- src/app.js
+++ src/app.js
@@ -1,1 +1,2 @@
 require('./components/CommentBox');
+require('file?name=[name].[ext]!./comments.json')
--- src/components/CommentForm.jsx
+++ src/components/CommentForm.jsx
@@ -3,16 +3,40 @@ import React, { PropTypes } from 'react';
 class CommentForm extends React.Component {
   state = {
     author: '',
+    text: ''
   }
 
+  handleAuthorChange = (e) => {
+    this.setState({
+      author: e.target.value
+    })
+  }
+
+  handleTextChange = (e) => {
+    this.setState({
+      text: e.target.value
+    })
+  }
 
   render() {
     return (
-      <form className='commentForm'>
-        <input placeholder='Your name' />
-        <input placeholder='Say something...' />
-        <button type='submit'>Post</button>
-      </form>
+      <div>
+        <p>author: {this.state.author}</p>
+        <p>text: {this.state.text}</p>
+        <form className='commentForm'>
+          <input
+            placeholder='Your name'
+            value={this.state.author}
+            onChange={this.handleAuthorChange}
+            />
+          <input
+            placeholder='Say something...'
+            value={this.state.text}
+            onChange={this.handleTextChange}
+          />
+          <button type='submit'>Post</button>
+        </form>
+      </div>
     )
   }
 }
